79e080c8a024b3ce0c9f3ae93ca485cbb0e66925
@paulywrath <127055825+paulywrath@users.noreply.github.com>
2024-07-22 15:10:02 -0400

    Remove comment and console log

--- contact-list/src/components/ContactList.jsx
+++ contact-list/src/components/ContactList.jsx
@@ -3,16 +3,9 @@ import { useState } from 'react'
 import { useEffect } from 'react' 
 import ContactRow from "./ContactRow";
 
-const dummyContacts = [
-  { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
-  { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
-  { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
-];
-
 export default function ContactList({setSelectedContactId}) { 
 
-  const [contacts, setContacts] = useState(dummyContacts);
-  console.log("Contacts: ", contacts);
+  const [contacts, setContacts] = useState([]);
 
   useEffect(() => {
     async function fetchContacts() {
